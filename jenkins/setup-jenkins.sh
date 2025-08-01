323fc90c606c831492855d5353b9d94708146e7b
FuhChu <you@example.com>
2025-07-31 20:11:21 -0600

    Added a while loop in the jenkins setup file

--- jenkins/setup-jenkins.sh
+++ jenkins/setup-jenkins.sh
@@ -16,5 +16,19 @@ docker run -d --name jenkins \
   jenkins/jenkins:lts
 
 # Output unlock password
-echo "Jenkins Initial Password:"
+#!/bin/bash
+
+echo "Waiting for Jenkins to be ready..."
+
+# Wait up to 90 seconds for Jenkins to finish initializing
+for i in {1..30}; do
+  if docker exec jenkins test -f /var/jenkins_home/secrets/initialAdminPassword; then
+    echo "Jenkins is ready!"
+    break
+  fi
+  echo "Jenkins not ready yet... ($i)"
+  sleep 3
+done
+
+# Now print the password
 docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
